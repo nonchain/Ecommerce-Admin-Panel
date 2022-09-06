afb4385080f08f129ff0e703a6806f4117cea268
progali007 <code.progali@gmail.com>
2022-09-06 13:03:30 +0430

    Build 404 Page

---
+++ src/pages/error404/Page404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './error.module.scss'
+
+const Page404 = () => {
+  return (
+    <div className={styles.page}>
+      You'r Lost body !
+    </div>
+  )
+}
+
+export default Page404
