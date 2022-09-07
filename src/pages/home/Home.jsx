e1c7f3337c34c46c9524003170e34d325763fbe2
progali007 <code.progali@gmail.com>
2022-09-07 14:01:46 +0430

    Add Navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import styles from './home.module.scss';
 
 const Home = () => {
   return (
     <div className={styles.home}>
+
       <Sidebar />
       <div className={styles.container}>
-         Content
+        <Navbar />
+        Content
       </div>
     </div>
   )
