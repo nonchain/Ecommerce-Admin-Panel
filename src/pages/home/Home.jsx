7446cd1f3040099e53a3f422ac634586f44617e1
progali007 <code.progali@gmail.com>
2022-09-09 12:45:42 +0430

    Add widgets to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Widget from '../../components/widget/Widget';
 import styles from './home.module.scss';
 
 const Home = () => {
@@ -8,9 +9,17 @@ const Home = () => {
     <div className={styles.home}>
 
       <Sidebar />
-      <div className={styles.container}>
+      <div className={styles.content}>
         <Navbar />
-        Content
+
+        <div className={styles.container}>
+          <div className={styles.widgets}>
+            <Widget type={'user'} diff={20} amount={100} />
+            <Widget type={'order'} diff={20} amount={100} />
+            <Widget type={'earning'} diff={-10} amount={100} />
+            <Widget type={'balance'} diff={20} amount={100} />
+          </div>
+        </div>
       </div>
     </div>
   )
