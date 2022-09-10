3de7846a7e0dbc36289627b707bb1a45168b0f69
progali007 <code.progali@gmail.com>
2022-09-10 21:27:53 +0430

    Repalced the Sidebar & Navbar Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Home from './pages/home/Home';
 import { Routes, Route, Link } from "react-router-dom";
+import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import List from './pages/list/List';
 import New from './pages/new/New';
@@ -11,26 +13,33 @@ import 'remixicon/fonts/remixicon.css';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path='/'>
-          <Route index element={<Home />} />
-          <Route path='login' element={<Login />} />
-          {/*----- USER -----*/}
-          <Route path='users'>
-            <Route index element={<List />} />
-            <Route path='new' element={<New />} />
-            <Route path=':userId' element={<Single />} />
-          </Route>
-          {/*----- PRODUCT -----*/}
-          <Route path='product'>
-            <Route index element={<List />} />
-            <Route path='new' element={<New />} />
-            <Route path=':productId' element={<Single />} />
-          </Route>
-          <Route path='*' element={<Page404 />} />
-        </Route>
-      </Routes>
+    <div className='app'>
+      <div></div>
+      <Sidebar />
+      <div className='content'>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/'>
+              <Route index element={<Home />} />
+              <Route path='login' element={<Login />} />
+              {/*----- USER -----*/}
+              <Route path='users'>
+                <Route index element={<List />} />
+                <Route path='new' element={<New />} />
+                <Route path=':userId' element={<Single />} />
+              </Route>
+              {/*----- PRODUCT -----*/}
+              <Route path='product'>
+                <Route index element={<List />} />
+                <Route path='new' element={<New />} />
+                <Route path=':productId' element={<Single />} />
+              </Route>
+              <Route path='*' element={<Page404 />} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
 
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,42 +1,36 @@
 import React from 'react';
-import Navbar from '../../components/navbar/Navbar';
 import Revenue from '../../components/revenue/Revenue';
 import RevenueChart from '../../components/revenue/RevenueChart';
-import Sidebar from '../../components/sidebar/Sidebar';
 import TableTransactions from '../../components/table/TableTransactions';
 import Widget from '../../components/widget/Widget';
 import styles from './home.module.scss';
 
 const Home = () => {
   const widgets = [
-    {id: 1, type: 'user', diff: 34, amount: 100},
-    {id: 2, type: 'order', diff: 20, amount: 90},
-    {id: 3, type: 'earning', diff: -10, amount: 100},
-    {id: 4, type: 'balance', diff: 2, amount: 120}
+    { id: 1, type: 'user', diff: 34, amount: 100 },
+    { id: 2, type: 'order', diff: 20, amount: 90 },
+    { id: 3, type: 'earning', diff: -10, amount: 100 },
+    { id: 4, type: 'balance', diff: 2, amount: 120 }
   ]
   return (
     <div className={styles.home}>
 
-      <Sidebar />
+
       <div className={styles.content}>
-        <Navbar />
+        <div className={styles.widgets}>
+          {widgets.map(widget => <Widget key={widget.id} type={widget.type} diff={widget.diff} amount={widget.amount} />)}
+        </div>
 
-        <div className={styles.container}>
-          <div className={styles.widgets}>
-            {widgets.map(widget => <Widget key={widget.id} type={widget.type} diff={widget.diff} amount={widget.amount} />)}
-          </div>
+        <div className={styles.revenue}>
+          <Revenue percentage={70} />
+          <RevenueChart />
+        </div>
 
-          <div className={styles.revenue}>
-            <Revenue percentage={70}/>
-            <RevenueChart />
-          </div>
+        <div className={styles.table}>
+          <h2>Last transactions</h2>
 
-          <div className={styles.table}>
-            <h2>Last transactions</h2>
-
-            <div>
-              <TableTransactions />
-            </div>
+          <div>
+            <TableTransactions />
           </div>
         </div>
       </div>
