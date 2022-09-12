b629602a7540c8dd7e047ac99fbc4b2e993e22a3
progali007 <code.progali@gmail.com>
2022-09-12 12:12:51 +0430

    Design Single page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import TableTransactions from '../../components/table/TableTransactions';
+import RevenueChart from '../../components/revenue/RevenueChart';
+import styles from './single.module.scss';
+import UserWidget from '../../components/widget/UserWidget';
+
+const data = [
+  { name: 'Jan', total: 400, },
+  { name: 'Feb', total: 320, },
+  { name: 'Mar', total: 224, },
+  { name: 'Apr', total: 278, },
+  { name: 'May', total: 189, },
+  { name: 'Jun', total: 239, },
+  { name: 'Jul', total: 203, },
+  { name: 'Aug', total: 239, },
+  { name: 'Sep', total: 312, },
+  { name: 'Oct', total: 278, },
+  { name: 'Nov', total: 326, },
+  { name: 'Dec', total: 346, }
+];
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className={styles.user__info}>
+      <div className={styles.info__body}>
+        <UserWidget />
+        <RevenueChart />
+      </div>
+
+      <div className={styles.transactions}>
+        <h2>Last transactions</h2>
+
+        <div>
+          <TableTransactions />
+        </div>
+      </div>
+    </div>
   )
 }
 
