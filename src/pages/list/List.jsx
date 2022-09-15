5dc4209db35a5d8da55ebc89c8cf1d0c1f40be2b
progali007 <code.progali@gmail.com>
2022-09-15 08:33:00 +0430

    Add link, Add new user

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,5 +1,6 @@
 import { style } from '@mui/system';
 import React from 'react'
+import { Link } from 'react-router-dom';
 import TableData from '../../components/table/TableData';
 
 import styles from './list.module.scss';
@@ -7,6 +8,12 @@ import styles from './list.module.scss';
 const List = () => {
   return (
     <div>
+      <Link to={'new'}>
+        <span className={styles.add__user}>
+          <i className="ri-add-line"></i>
+          Add user
+        </span>
+      </Link>
       <div className={styles.table}>
         <TableData />
       </div>
