1569ee956457ec5dc1fcb3050d531ce2703186e1
progali007 <code.progali@gmail.com>
2022-09-16 11:37:38 +0430

    Add Delete onClick functionality

--- src/components/button/ButtonDelete.jsx
+++ src/components/button/ButtonDelete.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './buttons_style.module.scss';
 
-const ButtonDelete = ({text}) => {
+const ButtonDelete = ({text, onClick}) => {
   return (
-    <button className={styles.button__del}>
+    <button className={styles.button__del} onClick={onClick}>
       {text}
     </button>
   )
--- src/components/table/TableData.jsx
+++ src/components/table/TableData.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from '../../data/usersData';
@@ -8,7 +8,7 @@ import ButtonDelete from '../button/ButtonDelete';
 
 const setData = ({ username, email, img }) => {
    const data = {
-      username : username,
+      username: username,
       email: email,
       img: img,
       phone: '09123456789',
@@ -17,29 +17,39 @@ const setData = ({ username, email, img }) => {
    return data;
 }
 
-const actionColumn = [
-   {
-      field: 'action',
-      headerName: 'Action',
-      width: 200,
-      renderCell(params) {
-         return (
-            <div style={{ display: 'flex', gap: '8px' }}>
-               <Link to={`${params.row.id}`} state={{ data: setData(params.row) }}>
-                  <ButtonPrimary text={'View'} />
-               </Link>
-               <ButtonDelete text={'Delete'} />
-            </div>
-         )
-      }
-   }
-]
+
 
 const TableData = () => {
+   const [row, setRow] = useState(userRows);
+
+   const deleteItem = (id) => {
+      setRow(
+         row.filter(item => item.id !== id)
+      )
+   }
+
+   const actionColumn = [
+      {
+         field: 'action',
+         headerName: 'Action',
+         width: 200,
+         renderCell(params) {
+            return (
+               <div style={{ display: 'flex', gap: '8px' }}>
+                  <Link to={`${params.row.id}`} state={{ data: setData(params.row) }}>
+                     <ButtonPrimary text={'View'} />
+                  </Link>
+                  <ButtonDelete text={'Delete'} onClick={()=> deleteItem(params.row.id)}/>
+               </div>
+            )
+         }
+      }
+   ];
+
    return (
       <div style={{ height: 520, width: '100%' }}>
          <DataGrid
-            rows={userRows}
+            rows={row}
             columns={userColumns.concat(actionColumn)}
             pageSize={7}
             rowsPerPageOptions={[7]}
